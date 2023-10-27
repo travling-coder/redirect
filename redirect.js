fb7a5549953f936b31f51ccd81a545b326587c98
emn178 <emn178@gmail.com>
2023-10-27 16:06:54 +0800

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -5,7 +5,7 @@ if (window['location'].href.startsWith('https://emn178.pixnet.net/blog/post/')) 
   meta.httpEquiv = 'refresh';
   meta.content = `0; url=${url}`;
   document.head.appendChild(meta);
-  document.querySelector('link[rel=canonical]').setAttribue('href', url);
+  document.querySelector('link[rel=canonical]').setAttribute('href', url);
   document.body.innerHTML = '<div style="text-align:center;font-size:48px;line-height:1.5">' + element.outerHTML + '</div>';
   // window['localhost'].href = url;
 }
