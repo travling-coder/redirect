a0bc1dfc5605229afbf97fe6b83439e820c5cce2
emn178 <emn178@gmail.com>
2023-10-27 15:58:19 +0800

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,9 +1,11 @@
 if (window['location'].href.startsWith('https://emn178.pixnet.net/blog/post/')) {
-  var element = jQuery('#move-to');
-  var url = element.attr('href');
-  var part = `http-equiv="refresh" content="0; url=${url}"`
-  jQuery('head').append(`<meta ${part}>`);
-  jQuery('link[rel=canonical]').attr('href', url);
-  jQuery('body').html('<div style="text-align:center;font-size:48px;line-height:1.5">' + element[0].outerHTML + '</div>');
+  var element = document.getElementById('move-to');
+  var url = element.getAttribute('href');
+  var meta = document.createElement('meta');
+  meta.httpEquiv = 'refresh';
+  meta.content = `0; url=${url}`;
+  document.head.appendChild(meta);
+  document.querySelector('link[rel=canonical]').setAttribue('href', url);
+  document.body.innerHTML = '<div style="text-align:center;font-size:48px;line-height:1.5">' + element.outerHTML + '</div>';
   // window['localhost'].href = url;
 }
