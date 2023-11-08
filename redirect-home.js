65e465f5f33619b7c07459bd196bc367e54d32c6
emn178 <emn178@gmail.com>
2023-11-08 11:54:54 +0800

    Create redirect-home.js

---
+++ redirect-home.js
@@ -0,0 +1,12 @@
+var element = document.getElementById('move-to');
+var url = element.getAttribute('href');
+var meta = document.createElement('meta');
+meta.httpEquiv = 'refresh';
+meta.content = `0; url=${url}`;
+document.head.appendChild(meta);
+document.querySelector('link[rel=canonical]').setAttribute('href', url);
+document.body.innerHTML = '<div style="text-align:center;font-size:48px;line-height:1.5">' + element.outerHTML + '</div>';
+// window['localhost'].href = url;
+document.querySelectorAll('head script').forEach(function(e) {
+  e.remove()
+});
